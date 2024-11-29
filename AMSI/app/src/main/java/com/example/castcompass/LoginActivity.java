3efac50efb30da341c66fba750a2f0f17e2d44e1
DiogoVieiSilva <64011236+Diogo0Silva@users.noreply.github.com>
2024-11-29 15:50:18 +0000

    NavBar Android

--- AMSI/app/src/main/java/com/example/castcompass/LoginActivity.java
+++ AMSI/app/src/main/java/com/example/castcompass/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.example.castcompass;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,56 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
 
+    public static final int TAMANHO_MINIMO_SENHA = 4;
+    public static final String EMAIL = "Email";
+
+    public EditText etEmail;
+    public EditText etPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+    }
+
+    public void onClickLogin(View view) {
+        //TODO: código para validar email e password
+        //usar o setError das TextViews para mostrar mensagens de erro
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+
+        if (!isEmailValid(email)) {
+            etEmail.setError("Email inválido");
+            return;
+        }
+
+        if (!isPasswordValid(password)) {
+            etPassword.setError("Tem de ter 4 ou mais Caracteres");
+            return;
+        }
+
+//        Toast.makeText(this,"Login efetuado com sucesso", Toast.LENGTH_LONG).show();
+//        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent(this, MenuMainActivity.class);
+        intent.putExtra(EMAIL, email);
+        startActivity(intent);
+        finish();
+    }
+
+    public boolean isEmailValid(String email) {
+        if (email == null) {
+            return false;
+        }
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    public boolean isPasswordValid(String password) {
+        if (password == null) {
+            return false;
+        }
+        return password.length() >= TAMANHO_MINIMO_SENHA;
     }
 }
---
+++ AMSI/app/src/main/java/com/example/castcompass/MenuMainActivity.java
@@ -0,0 +1,102 @@
+package com.example.castcompass;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MenuMainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private NavigationView navigationView;
+    private DrawerLayout drawer;
+    private FragmentManager fragmentManager;
+    private String email = "Sem Email!";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navView);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.ndOpen, R.string.ndClose);
+        toggle.syncState();
+        drawer.addDrawerListener(toggle);
+
+        carregarCabecalho(); //TODO:criar método
+        navigationView.setNavigationItemSelectedListener(this);
+        fragmentManager = getSupportFragmentManager();
+        carregarFragmentoInicial();
+    }
+
+    private boolean carregarFragmentoInicial() {
+        Menu menu = navigationView.getMenu();
+        MenuItem item = menu.getItem(0);
+        item.setChecked(true);
+        return onNavigationItemSelected(item);
+    }
+
+    private void carregarCabecalho() {
+        email = getIntent().getStringExtra(LoginActivity.EMAIL);
+        SharedPreferences sharedPrefUser = getSharedPreferences("DADOS_USER", Context.MODE_PRIVATE);
+
+        if (email != null) {
+            SharedPreferences.Editor editor = sharedPrefUser.edit();
+            editor.putString("EMAIL", email);
+            editor.apply();
+        } else {
+            email = sharedPrefUser.getString("EMAIL", "Sem Email!");
+        }
+
+        View headerView = navigationView.getHeaderView(0);
+        TextView nav_tvEmail = headerView.findViewById(R.id.tvEmail);
+        nav_tvEmail.setText(email);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        if (item.getItemId() == R.id.navLista) {
+            setTitle(item.getTitle());
+        } else if (item.getItemId() == R.id.navGrelha) {
+            setTitle(item.getTitle());
+//            fragment = new GrelhaLivrosFragment();
+        } else {
+            enviarEmail();
+        }
+
+        if (fragment != null) {
+            fragmentManager.beginTransaction().replace(R.id.contentFragment, fragment).commit();
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void enviarEmail() {
+        //TODO: intent implicito ACTION_SEND
+    }
+}
