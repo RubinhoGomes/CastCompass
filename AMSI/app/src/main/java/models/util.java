72a23d18a7b42fbb58b55c43d13163ff79ccfee1
Rubinho Gomes <ruben.gomes16@hotmail.com>
2024-12-05 23:13:05 +0000

    👔 Added the necessary files to receive from the API 👔
    Not the core code to the files, just the files to start organzing the crew to code everything
    Added some files.

---
+++ AMSI/app/src/main/java/models/util.java
@@ -0,0 +1,13 @@
+package models;
+
+public class util {
+
+    public static String getServerUrl() {
+        return "Projeto/CastCompass/PLSI/CastCompass/backend/";
+    }
+
+    public static boolean isConnected(){
+        return true;
+    }
+
+}
