40e59421e06a9f96a35e533298761cf1edbef35b
DiogoVieiSilva <64011236+Diogo0Silva@users.noreply.github.com>
2024-12-06 15:22:37 +0000

    api

--- PLSI/CastCompass/backend/config/main.php
+++ PLSI/CastCompass/backend/config/main.php
@@ -46,6 +46,7 @@ return [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                ['class' => 'yii\rest\UrlRule','controller' => 'api/user'],
             ],
         ],
     ],
