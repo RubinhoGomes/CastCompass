b065627af3db4c48cafd79389d2ab36f7ae3d228
DiogoVieiSilva <64011236+Diogo0Silva@users.noreply.github.com>
2025-01-08 20:42:02 +0000

    Test favoritos

---
+++ PLSI/CastCompass/frontend/tests/functional/favoritosCest.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace frontend\tests\Functional;
+
+use frontend\tests\FunctionalTester;
+use common\models\User;
+
+class favoritosCest
+{
+    public function _before(FunctionalTester $I)
+    {
+        $I->seeRecord(User::className(), ['username' => 'Client']);
+        $I->amLoggedInAs(User::findOne(['username' => 'Client']));
+    }
+
+    // tests
+    public function tryToTest(FunctionalTester $I)
+    {
+        $I->amOnPage('/site/shop');
+        $I->click('.fa-heart');
+        $I->amOnPage('/favoritos/index');
+        $I->see('Products');
+        $I->click('.fa');
+
+    }
+}
